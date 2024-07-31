ebb3d5203ffd1358ca1a5e8ec778acc710ffad69
mrokuu <mroczkowskiadammm@gmail.com>
2024-07-31 19:40:31 +0200

    removed code

--- infrastructure/src/main/java/org/example/repository/user/UserCommandRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository.user;
-
-import org.example.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserCommandRepository extends JpaRepository<User, UUID> {
-}
